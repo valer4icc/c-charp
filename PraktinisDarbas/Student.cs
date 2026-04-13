7dd4521ce43c6365a4ed7e57464387c14511bff7
Valentin <valentrainn@gmail.com>
2026-04-13 21:12:44 +0300

    Add files via upload

---
+++ PraktinisDarbas/Student.cs
@@ -0,0 +1,11 @@
+public class Student
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+
+    public Student(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+}
